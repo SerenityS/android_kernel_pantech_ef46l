8ab91f7578a80615e0bfe44f7a983f21941b66c8
diszell2008 <listening2009@gmail.com>
2015-01-10 12:54:22 +0700

    Test

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+	#!/bin/bash
 ###############################################################################
 #
 #                           Kernel Build Script 
@@ -9,20 +9,11 @@
 # set toolchain
 ##############################################################################
 export ARCH=arm
-export CROSS_COMPILE=/working1/sdv/thinhhoang/android/arm-eabi-4.6/bin/arm-eabi-
+export CROSS_COMPILE=/home/diszell2008/arm-eabi-4.7/bin/arm-eabi-
 
 ##############################################################################
 # make zImage
 ##############################################################################
-mkdir -p ../obj_830/KERNEL_OBJ/
-make O=../obj_830/KERNEL_OBJ msm8960_ef46l_tp30_user_defconfig
-make -j8 O=../obj_830/KERNEL_OBJ
-
-##############################################################################
-# Copy Kernel Image
-##############################################################################
-if [ -f ./obj/KERNEL_OBJ/arch/arm/boot/zImage ]
-then
-    cp -f ./obj/KERNEL_OBJ/arch/arm/boot/zImage ./
-fi
+make  msm8960_ef46l_tp30_user_defconfig
+make -j16 zImage
 
